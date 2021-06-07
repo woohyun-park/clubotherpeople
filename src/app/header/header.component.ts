1428750e8d0aa6603fd3b1a1b8c4328b211464f9
Woohyun Park <woohyunpark.mail@gmail.com>
2021-06-07 13:05:55 +0900

    REFACTOR refactor orbit related methods

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,7 +13,7 @@ export class HeaderComponent implements OnInit {
   constructor(private appService: AppService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.animateOrbit();
+    this.changeOrbitColor();
   }
 
   //로고를 눌렀을때 홈페이리자면 페이지를 리로드. 다른 페이지라면 홈페이지로 이동
@@ -26,17 +26,11 @@ export class HeaderComponent implements OnInit {
     window.scrollTo(0, 0);
   }
 
-  animateOrbit(): void {
+  changeOrbitColor(): void {
     let count = 0;
     setInterval(() => {
       const path = <HTMLElement>(document.querySelector(".center__orbit--path"));
-      if(count % 3 === 0){
-        path.style.fill = this.appService.actionColors[count % 3];
-      } else if(count % 3 === 1){
-        path.style.fill = this.appService.actionColors[count % 3];
-      } else {
-        path.style.fill = this.appService.actionColors[count % 3];
-      }
+      path.style.fill = this.appService.actionColors[count % 3];
       count++;
     }, 1000);
   }
@@ -50,22 +44,17 @@ export class HeaderComponent implements OnInit {
     const square = <HTMLElement>(document.querySelector(".center__text--square"));
     const triangle = <HTMLElement>(document.querySelector(".center__text--triangle"));
     const circle = <HTMLElement>(document.querySelector(".center__text--circle"));
-    square.style.transform = "scale(1.1)";
-    square.style.color = this.appService.actionColors[0];
+
+    this.changeTransformAndColor(square, "scale(1.1)", this.appService.actionColors[0]);
     const timer = setTimeout(() => {
       //혹시라도 전단계가 실행이 되지 않을 수 있으니, 전단계인 square도 다시한번 상태변경
-      square.style.transform = "scale(1.1)";
-      square.style.color = this.appService.actionColors[0];
-      triangle.style.transform = "scale(1.15)";
-      triangle.style.color = this.appService.actionColors[1];
+      this.changeTransformAndColor(square, "scale(1.1)", this.appService.actionColors[0]);
+      this.changeTransformAndColor(triangle, "scale(1.15)", this.appService.actionColors[1]);
       setTimeout(() => {
         //역시 전단계가 실행이 되지 않을 수 있으니, 전단계와 전전단계 모두 다시한번 상태변경
-        square.style.transform = "scale(1.1)";
-        square.style.color = this.appService.actionColors[0];
-        triangle.style.transform = "scale(1.15)";
-        triangle.style.color = this.appService.actionColors[1];
-        circle.style.transform = "scale(1.1) skewX(-15deg)";
-        circle.style.color = this.appService.actionColors[2];
+        this.changeTransformAndColor(square, "scale(1.1)", this.appService.actionColors[0]);
+        this.changeTransformAndColor(triangle, "scale(1.15)", this.appService.actionColors[1]);
+        this.changeTransformAndColor(circle, "scale(1.1) skewX(-15deg)", this.appService.actionColors[2]);
       }, 100);
     }, 100);
   }
@@ -77,21 +66,21 @@ export class HeaderComponent implements OnInit {
     const square = <HTMLElement>(document.querySelector(".center__text--square"));
     const triangle = <HTMLElement>(document.querySelector(".center__text--triangle"));
     const circle = <HTMLElement>(document.querySelector(".center__text--circle"));
-    circle.style.transform = "scale(1) skewX(-15deg)";
-    circle.style.color = this.appService.baseColor;
+
+    this.changeTransformAndColor(circle, "scale(1) skewX(-15deg)", this.appService.baseColor);
     const timer = setTimeout(() => {
-      circle.style.transform = "scale(1) skewX(-15deg)";
-      circle.style.color = this.appService.baseColor;
-      triangle.style.transform = "scale(1)";
-      triangle.style.color = this.appService.baseColor;
+      this.changeTransformAndColor(circle, "scale(1) skewX(-15deg)", this.appService.baseColor);
+      this.changeTransformAndColor(triangle, "scale(1)", this.appService.baseColor);
       setTimeout(() => {
-        circle.style.transform = "scale(1) skewX(-15deg)";
-        circle.style.color = this.appService.baseColor;
-        triangle.style.transform = "scale(1)";
-        triangle.style.color = this.appService.baseColor;
-        square.style.transform = "scale(1)";
-        square.style.color = this.appService.baseColor;
+        this.changeTransformAndColor(circle, "scale(1) skewX(-15deg)", this.appService.baseColor);
+        this.changeTransformAndColor(triangle, "scale(1)", this.appService.baseColor);
+        this.changeTransformAndColor(square, "scale(1)", this.appService.baseColor);
       }, 100);
     }, 100);
+  }
+
+  changeTransformAndColor(shape, transform, color){
+    shape.style.transform = transform;
+    shape.style.color = color;
   }
 }
